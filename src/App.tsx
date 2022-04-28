07049a2ce861be5fccd956c33d38e8828b32ebc7
topsart <64708421+topsart@users.noreply.github.com>
2022-04-28 18:33:11 +0300

    fix yml

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React deploy test in PR
+          Learn React deploy
         </a>
       </header>
     </div>
